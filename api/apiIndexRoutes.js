8c39f6cf8817333d2596ee439d9d7962a9e960e2
username <156992063+Ketandetrfyd13rd3ydf7h@users.noreply.github.com>
2025-03-06 09:28:48 +0530

    Status added

--- api/apiIndexRoutes.js
+++ api/apiIndexRoutes.js
@@ -3,55 +3,55 @@ const router = express.Router();
 const path = require('path'); 
 
 router.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/welcome.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/welcome.html'))
 })
 
 router.get('/login', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/login.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/login.html'))
 })
 
 router.get('/home', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/home.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/home.html'))
 })
 
 router.get('/discover', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/discover.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/discover.html'))
 })
 
 router.get('/events', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/events.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/events.html'))
 })
 
 router.get('/prepare', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/prepare.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/prepare.html'))
 })
 
 router.get('/careers', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/careers.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/careers.html'))
 })
 
 router.get('/community', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/community.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/community.html'))
 })
 
 router.get('/courses', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/courses.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/courses.html'))
 })
 
 router.get('/rankings', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/ranking.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/ranking.html'))
 })
 
 router.get('/Resources', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/resources.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/resources.html'))
 })
 
 router.get('/feestructure', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/feestructure.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/feestructure.html'))
 })
 
 router.get('/register', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../views/signUp.html'))
+    res.status(200).sendFile(path.join(__dirname, '../views/signUp.html'))
 })
 
 module.exports = router;
