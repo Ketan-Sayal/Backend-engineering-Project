bcb3a43db52fe3ebe339b934ace9228725000aa4
username <156992063+Ketandetrfyd13rd3ydf7h@users.noreply.github.com>
2025-04-11 11:38:28 +0530

    changed spellings

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.listen(80, ()=>{
 }).on("error", (err)=>{
     if(err.code === "EADDRINUSE"){
         app.listen(3000, ()=>{
-            console.log("Port 80 is busy and server is tring to run it on port 3000");
+            console.log("Port 80 is busy and server is trying to run it on port 3000");
             console.log("Server is running at port 3000");
             console.log("\nhttp://localhost:3000");
         });
