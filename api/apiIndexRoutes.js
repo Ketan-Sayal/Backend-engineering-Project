d0215999f282958d3be053d5740c8bc2b46321e4
username <156992063+Ketandetrfyd13rd3ydf7h@users.noreply.github.com>
2025-04-17 10:25:47 +0530

    Logout functionality added

--- api/apiIndexRoutes.js
+++ api/apiIndexRoutes.js
@@ -12,52 +12,52 @@ router.get('/login', (req, res)=>{
 })
 
 router.get('/home', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('home.ejs', {name:username});
 })
 
 router.get('/discover', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('discover.ejs', {name:username});
 })
 
 router.get('/events', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('events.ejs', {name:username}, );
 })
 
 router.get('/prepare', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('prepare.ejs', {name:username});
 })
 
 router.get('/careers', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('careers.ejs', {name:username});
 })
 
 router.get('/community', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('community.ejs', {name:username});
 })
 
 router.get('/courses', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('courses.ejs', {name:username});
 })
 
 router.get('/rankings', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('ranking.ejs', {name:username});
 })
 
 router.get('/Resources', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('resources.ejs', {name:username});
 })
 
 router.get('/feestructure', getUserData, (req, res)=>{
-    const {username} = req.user;
+    const {username} = req?.user;
     res.status(200).render('feestructure.ejs', {name:username});
 })
 
@@ -65,5 +65,10 @@ router.get('/register', (req, res)=>{
     res.status(200).render('signUp.ejs');
 })
 
+
+router.get('/logout', getUserData, (req, res)=>{
+    res.clearCookie("userData")
+    .redirect('/');
+})
 
 module.exports = router;
